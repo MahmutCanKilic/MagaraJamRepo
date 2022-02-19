8504135139e612c5f9207153c6fac55403714566
Murphyqq <89387909+Murphyqq@users.noreply.github.com>
2022-02-20 02:16:51 +0300

    Push4
    Attacking, Walking, Running, Jumping, TimeTravelSpell, Idle animations added.

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator animator;
-    
+    private ThirdPersonMovement movement;
+    private AnimationClip clip;
     private void Start()
     {
-        animator = GetComponent<Animator>();    
+        animator = GetComponent<Animator>();
+        movement = FindObjectOfType<ThirdPersonMovement>();
     }
     private void Update()
-    {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+    {       
+        if (movement.isMoving == true)
         {
-            animator.SetTrigger("Walk");
+            animator.SetBool("Walking", true);
+            Invoke("Run", 0.3f);
+            Debug.Log(animator.GetBool("Walking"));
         }
+        else
+        {
+            animator.SetBool("Walking", false);
+            animator.SetBool("Running", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetTrigger("Attack");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("TimeTravel");
+        }
+        
+    }
+    private void Run()
+    {
+        animator.SetBool("Running", true);
     }
 }
